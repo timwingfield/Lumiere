5c416fb43cdbaa2628abab99efe8d1ac9a88bc42
Tim Wingfield <tim@timwingfield.com>
2010-09-04 11:36:25 -0400

    updated db file with specific connection info for mongohq

--- lib/db.rb
+++ lib/db.rb
@@ -4,11 +4,10 @@ module Lumiere
   module Database
     def self.init_mongo
       if ENV['MONGOHQ_URL']
-        connection = Mongo::Connection.new(ENV['MONGOHQ_URL'])
-        Mongoid.database = connection.db('LumiereStaging')
+        Mongoid.database = Mongo::Connection.new("flame.mongohq.com", 27029).db('LumiereStaging') 
+        Mongoid.database.authenticate('lumiere', 'G@st0n')
       else
-        connection = Mongo::Connection.new("localhost")
-        Mongoid.database = connection.db('LumiereDev')
+        Mongoid.database = Mongo::Connection.new("localhost").db('LumiereDev')
       end
     end
   end
