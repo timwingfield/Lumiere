5825957d7e7127db1ae18cb8d53de6ddfa9f922b
Tim Wingfield <tim@timwingfield.com>
2010-09-04 14:30:50 -0400

    put uncommented meal attr read/write code. won't work on heroku this way, need to fix

--- lib/meal_helpers.rb
+++ lib/meal_helpers.rb
@@ -6,14 +6,14 @@ module MealHelpers
     html << "<li>#{meal.meal_type.gsub("_", " ").capitalize}</li>"
     html << "<li>Time: #{meal.time}</li>"
 
-    # Meal.send("#{meal.meal_type}_fields".to_sym).each do |field|
-    #   value = meal.read_attribute(field.to_sym)
+    Meal.send("#{meal.meal_type}_fields".to_sym).each do |field|
+      value = meal.read_attribute(field.to_sym)
 
-    #   html << "<li>"
-    #   html << "<span>#{field.gsub("_", " ").capitalize}: </span>"
-    #   html << "<span>#{value}</span>"
-    #   html << "</li>"
-    # end
+      html << "<li>"
+      html << "<span>#{field.gsub("_", " ").capitalize}: </span>"
+      html << "<span>#{value}</span>"
+      html << "</li>"
+    end
 
     html << "<li>Notes: #{meal.notes}</li>"
     html << "</ul>"
