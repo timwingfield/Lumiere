7e7ecda585ee5e9a081c2e9e472716fb69b96045
Tim Wingfield <tim@timwingfield.com>
2014-02-28 18:12:30 -0500

    Update db.rb
    Removed old db credentials

--- lib/db.rb
+++ lib/db.rb
@@ -3,12 +3,7 @@ require 'sinatra/base'
 module Lumiere
   module Database
     def self.init_mongo
-      if ENV['MONGOHQ_URL']
-        Mongoid.database = Mongo::Connection.new("flame.mongohq.com", 27029).db('LumiereStaging') 
-        Mongoid.database.authenticate('lumiere', 'G@st0n')
-      else
-        Mongoid.database = Mongo::Connection.new("localhost").db('LumiereDev')
-      end
+      Mongoid.database = Mongo::Connection.new("localhost").db('LumiereDev')
 
       Mongoid.configure do |config|
         config.allow_dynamic_fields = true 
